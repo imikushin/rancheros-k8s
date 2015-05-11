5df506b05f7e453dbd423d93ab99255e5919067c
Ivan Mikushin <i.mikushin@gmail.com>
2015-05-11 16:11:06 +0500

    rm docker0 nic before using flannel bridge ip

--- src/docker/rancheros-k8s/bin/docker-conf.sh
+++ src/docker/rancheros-k8s/bin/docker-conf.sh
@@ -3,4 +3,6 @@ set -ex
 
 . /var/run/flannel/subnet.env
 
-ros c set user_docker.args "[docker, -d, -s, overlay, -G, docker, --bip=${FLANNEL_SUBNET}, --mtu=${FLANNEL_MTU}]"
+ros c set user_docker.args "[docker, -d, -s, overlay, -G, docker, -H, unix:///var/run/docker.sock, --bip=${FLANNEL_SUBNET}, --mtu=${FLANNEL_MTU}]"
+
+ip link delete docker0
